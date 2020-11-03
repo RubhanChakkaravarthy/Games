4f93791df90f3d034488fa5235614b1a0a06a7ef
Jothi Rubhan Chakkaravarthy <chakkaravarthy100@outlook.com>
2020-11-03 15:49:27 +0530

    assignment 8 and updated git ignore file

---
+++ Helicopter/Assets/Resources/Scripts/CollectableSpawner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectableSpawner : MonoBehaviour {
+
+	public GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start () {
+
+		// infinite coin spawning function, asynchronous
+		StartCoroutine(SpawnCoins());
+		StartCoroutine(SpawnDiamonds());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator SpawnCoins() {
+		while (true) {
+
+			// number of coins we could spawn vertically
+			int coinsThisRow = Random.Range(1, 4);
+
+			// instantiate all coins in this row separated by some random amount of space
+			for (int i = 0; i < coinsThisRow; i++) {
+				Instantiate(prefabs[0], new Vector3(26, Random.Range(-10, 10), 13.5f), Quaternion.identity);
+			}
+
+			// instantiate a diamond rarely
+//			Instantiate(prefabs[1], new Vector3(26, Random.Range(-10, 10), 13.5f), Quaternion.identity);
+
+			// pause 1-5 seconds until the next coin spawns
+			yield return new WaitForSeconds(Random.Range(1, 5));
+		}
+	}
+
+	IEnumerator SpawnDiamonds() {
+		while (true) {
+
+			// prefabs[1] - Diamond Prefab
+			Instantiate(prefabs[1], new Vector3(26, Random.Range(-10, 10), 13.5f), Quaternion.identity);
+
+			// pause 10-15 seconds until next Diamond spawns
+			yield return new WaitForSeconds(Random.Range(10, 15));
+		}
+	}
+}
---
+++ Helicopter/Assets/Resources/Scripts/SkyscraperSpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyscraperSpawner : MonoBehaviour {
+
+	public GameObject[] prefabs;
+	private static float BaseSpeed = 10f;
+	public static float speed;
+
+	// Use this for initialization
+	void Start () {
+
+		// reset the speed to base speed when scene reloads
+		speed = BaseSpeed;
+
+		// aysnchronous infinite skyscraper spawning
+		StartCoroutine(SpawnSkyscrapers());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator SpawnSkyscrapers() {
+		while (true) {
+
+			// create a new skyscraper from prefab selection at right edge of screen
+			Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-20, -16), 13.5f),
+				Quaternion.identity);
+
+			// randomly increase the speed by 1
+			if (Random.Range(1, 4) == 1) {
+				speed += 1f;
+			}
+
+			// wait between 1-5 seconds for a new skyscraper to spawn
+			yield return new WaitForSeconds(Random.Range(2, 6));
+		}
+	}
+}
